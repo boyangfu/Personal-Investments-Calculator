4be6be4bc8b61cb02dda35299838cecf510b7cf1
Boyang <boyangfu@gmail.com>
2018-05-28 05:59:59 -0700

    Compound interest calc

--- test.php
+++ test.php
@@ -3,10 +3,27 @@
   <title>PHP-Test</title>
  </head>
  <body>
-  <?php 
-   echo '<p>Hello World!</p>';
+  <?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "password";
+
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=mysql", $username, $password);
+		// set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo "Connected successfully";
+		}
+	catch(PDOException $e)
+		{
+		echo "Connection failed: " . $e->getMessage();
+		} 
+   echo '<p>Net worth</p>';
+   $starting_capital = 500000
+   $annual_growth = 1.07
    for ( $i = 0; $i < 10; $i++){
-    echo '<p>Line '.($i+1).'</p>';
+    echo '<p>Year '.($i+1).': '.$starting_capital.'</p>';
+	$starting_capital = $starting_capital * $annual_growth
    }
   ?>
  </body>
