fcc3a30a48bdddac3da7bca28f20d3f53dbf9b2a
Boyang <boyangfu@gmail.com>
2018-05-28 00:41:01 -0700

    Testing tortoise git

--- test.php
+++ test.php
@@ -3,6 +3,11 @@
   <title>PHP-Test</title>
  </head>
  <body>
-  <?php echo '<p>Hello World!</p>'; ?>
+  <?php 
+   echo '<p>Hello World!</p>';
+   for ( $i = 0; $i < 10; $i++){
+    echo '<p>Line '.($i+1).'</p>';
+   }
+  ?>
  </body>
 </html>
